50fa7cdada721f570d70e0be57567e62417290e5
NellyBtn <butenkonelly93@gmail.com>
2021-04-23 00:06:07 +0000

    Clean the code, add more comments for readability

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
 
+  // SUBSTRACTING CHARS FROM THE COUNTER
   $('#tweet-text').on('input', function() {
-    const text = $(this).val();//input
-    let counter = $(this).parent().parent().children('.submit').children('.counter');
+    const text = $(this).val(); //input
+    let counter = $(this).parent().parent().children('.submit').children('.counter'); // traversing to find the counter
     const currentNumOfChars = text.length;
     let charsLeft = 140 - currentNumOfChars;
     counter.text(charsLeft);
 
-
-    if ( charsLeft < 0) {
-      counter.css('color', 'red')
+    // SET UP THE COLOR
+    if (charsLeft < 0) {
+      counter.css('color', 'red');
     } else {
-      counter.css('color', '#0f0e00')
+      counter.css('color', '#0f0e00');
     }
 
   });
